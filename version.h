cf984bcea4262f6127ca8d35a083e9fb95ad4d1e
Ross Smith II <ross@smithii.com>
2015-05-20 20:58:13 -0700

    Update Makefile

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #define VER_INTERNAL_NAME 		"PESetEnv"
 #define	VER_FILE_DESCRIPTION	"Set environment in BartPE"
 #define VER_MAJOR				1
-#define VER_MINOR				5
-#define VER_STRING2				"1.5"
+#define VER_MINOR				6
+#define VER_STRING2				"1.6"
 
 #include "ver_defaults.h"
